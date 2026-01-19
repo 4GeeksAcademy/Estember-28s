d24de46599acc1756fa744638e035b3c2d9c6b8d
Estember Sanchez Perez <sanchez17p@gmail.com>
2026-01-19 11:13:06 +0000

    al fin

---
+++ src/sql/queries copy.sql
@@ -0,0 +1,110 @@
+-- PLEASE READ THIS BEFORE RUNNING THE EXERCISE
+
+-- ⚠️ IMPORTANT: This SQL file may crash due to two common issues: comments and missing semicolons.
+
+-- ✅ Suggestions:
+-- 1) Always end each SQL query with a semicolon `;`
+-- 2) Ensure comments are well-formed:
+--    - Use `--` for single-line comments only
+--    - Avoid inline comments after queries
+--    - Do not use `/* */` multi-line comments, as they may break execution
+
+-- -----------------------------------------------
+-- queries.sql
+-- Complete each mission by writing your SQL query
+-- directly below the corresponding instruction
+-- -----------------------------------------------
+
+SELECT * FROM regions;
+SELECT * FROM species;
+SELECT * FROM climate;
+SELECT * FROM observations;
+
+--Nivel 1
+-- MISSION 1
+-- Your query here;
+SELECT * FROM observations
+LIMIT 10;
+
+-- MISSION 2
+-- Your query here;
+SELECT DISTINCT region_id FROM observations;
+
+
+-- MISSION 3
+-- Your query here;
+SELECT COUNT(DISTINCT species_id) AS total_species FROM observations;
+
+
+-- MISSION 4
+-- Your query here;
+SELECT * FROM observations
+WHERE region_id = 2;
+
+
+
+-- MISSION 5
+-- Your query here;
+SELECT * FROM observations
+WHERE observation_date = '1998-08-08';
+
+Nivel 2 
+-- MISSION 6
+-- Your query here;
+SELECT region_id, COUNT(*) AS total_observations
+FROM observations
+GROUP BY region_id
+ORDER BY total_observations DESC;
+
+
+-- MISSION 7
+-- Your query here;
+SELECT species_id, COUNT(*) AS total_records
+FROM observations
+GROUP BY species_id
+ORDER BY total_records DESC
+LIMIT 5;
+
+
+-- MISSION 8
+-- Your query here;
+SELECT species_id, COUNT(*) AS observation_count
+FROM observations
+GROUP BY species_id
+HAVING observation_count < 5
+ORDER BY observation_count ASC;
+-- MISSION 9
+-- Your query here;
+SELECT observer, COUNT(*) AS total
+FROM observations
+GROUP BY observer
+ORDER BY total DESC;
+
+--Nivel 3
+-- MISSION 10
+-- Your query here;
+SELECT observations.id, regions.name AS region_name, observations.observation_date
+FROM observations
+JOIN regions ON observations.region_id = regions.id;
+-- MISSION 11
+-- Your query here;
+SELECT observations.id, species.scientific_name
+FROM observations
+JOIN species ON observations.species_id = species.id;
+-- MISSION 12
+-- Your query here;
+SELECT regions.name AS region, species.scientific_name, COUNT(*) AS total
+FROM observations
+JOIN species ON observations.species_id = species.id
+JOIN regions ON observations.region_id = regions.id
+GROUP BY region, species.scientific_name
+ORDER BY region, total DESC;
+ --Nivel 4
+-- MISSION 13
+-- Your query here;
+
+-- MISSION 14
+-- Your query here;
+
+-- MISSION 15
+-- Your query here;
